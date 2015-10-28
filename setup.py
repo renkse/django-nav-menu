18682eaeb49ef5352703541278add5460dc58bba
eternalfame <eternalfame@mail.ru>
2015-10-28 17:11:47 +1000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -8,15 +8,15 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nav-menu',
-    version='0.1.3',
-    packages=['menu'],
+    version='0.1.4',
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',
     description='A Django app for creating the simplest menu trees in admin panel',
     long_description=README,
     url='https://github.com/renkse/django-nav-menu',
-    download_url='https://github.com/renkse/django-nav-menu/tarball/0.1.1',
-    author='renkse',
+    download_url='https://github.com/renkse/django-nav-menu/tarball/0.1.4',
+    author='renkse, eternalfame',
     author_email='solomon_art@mail.ru',
     classifiers=[
 	'Development Status :: 4 - Beta',
@@ -26,9 +26,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+    ], requires=["django", 'feincms', "django-mptt"]
 )
