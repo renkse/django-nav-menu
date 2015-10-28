d48e2ee3f5d493f2f1241e0a25a7d20b886b42fb
eternalfame <eternalfame@mail.ru>
2015-10-28 17:11:10 +1000

    info variables

--- menu/apps.py
+++ menu/apps.py
@@ -1,10 +1,14 @@
 # coding=utf-8
+__author__ = "eternalfame"
+
 try:
     from django.apps import AppConfig
 except ImportError:
     pass
 else:
+    from django.utils.translation import ugettext_lazy as _
+
     class MenuConfig(AppConfig):
         name = u"menu"
         label = u"menu"
-        verbose_name = u"Меню"
+        verbose_name = _(u"menu")
